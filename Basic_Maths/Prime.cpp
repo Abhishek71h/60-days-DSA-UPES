9a1694e20db39c78293076bbacded04b94111093
Abhishek71h <abhishek71w@gmail.com>
2025-08-06 08:41:39 +0530

    Basic Maths in dsa

---
+++ Basic_Maths/Prime.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<bool>Sieve(int n) {
+    // create a sieve array of n size telling isPrime
+    vector<bool>sieve(n+1, true);
+    sieve[0] = false;
+    sieve[1] = false;
+    for(int i=2; i<=n; i++) {
+        if(sieve[i] == true) {
+            int j = i*2;
+            while(j<=n) {
+                sieve[j] = false;
+                j += i;
+            }
+        }
+    }
+    return sieve;
+}
+int main() {
+    vector<bool>sieve = Sieve(25);
+    for(int i=0; i<=25; i++) {
+        if(sieve[i] == true) {
+            cout<<i<<" ";
+        }
+    }
+    return 0;
+}
+
+// optimization -> (inner loop) && (outer loop) T.C -> O(nlog(logn))
+
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<bool>Sieve(int n) {
+    // create a sieve array of n size telling isPrime
+    vector<bool>sieve(n+1, true);
+    sieve[0] = false;
+    sieve[1] = false;
+    for(int i=2; i*i<=n; i++) {
+        if(sieve[i] == true) {
+            int j = i*i;
+            while(j<=n) {
+                sieve[j] = false;
+                j += i;
+            }
+        }
+    }
+    return sieve;
+}
+int main() {
+    vector<bool>sieve = Sieve(25);
+    for(int i=0; i<=25; i++) {
+        if(sieve[i] == true) {
+            cout<<i<<" ";
+        }
+    }
+    return 0;
+}
