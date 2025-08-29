d138d19d6c12a9a3d0c1b1f80df5d21f7f2118d1
Abhishek71h <abhishek71w@gmail.com>
2025-08-29 09:57:08 +0530

    Sliding Window Added

---
+++ Sliding Window/MaximumSum.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<limits.h>
+using namespace std;
+int maximumSumSubarray(vector<int>& arr, int n, int k) {
+    /* Brute-force Approach
+    int maxSum = INT_MIN;
+    for(int i=0; i<=n-k; i++) {
+        int sum = 0;
+        for(int j=i; j<i+k; j++) {
+            sum = sum + arr[j];
+        }
+        maxSum = max(maxSum, sum);
+    }
+    cout << "Maximum Sum in Subarray of size k is: " << maxSum;
+    */
+
+    // Optimize Approach using Sliding Window Approach
+    int maxSum = INT_MIN;
+    int prevSum = 0;
+    for(int i=0; i<k; i++) {
+        prevSum = prevSum + arr[i];
+    }
+    maxSum = prevSum;
+    int i = 1;
+    int j = k;
+    while(j < n) {
+        int currSum = prevSum + arr[j] - arr[i-1];
+        if(currSum > maxSum) {
+            maxSum = currSum;
+        }
+        prevSum = currSum;
+        i++;
+        j++;
+    }
+    cout << "Maximum Sum in Subarray of size k is: " << maxSum;
+}
+int main() {
+    vector<int>arr = {7,1,2,5,8,4,9,3,6};
+    int n = arr.size();
+    int k = 3;
+    return maximumSumSubarray(arr, n, k);
+}
