f41de05464f15ace77ea469efa7fb5fe0571f032
Abhishek71h <abhiseema5057@gmail.com>
2025-09-16 21:30:33 +0530

    Update

--- Basic_Maths/GCD.cpp
+++ Basic_Maths/GCD.cpp
@@ -40,3 +40,13 @@ class Solution {
     }
 };
 
+// Faster Way -> Euclidean algorithm with modulo
+int gcd(int a, int b) {
+    while(b != 0) {
+        int temp = a % b; 
+        a = b;
+        b = temp;
+    }
+    return a;
+}
+
