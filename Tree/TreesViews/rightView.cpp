54ece5b7bd9eb5d05cc8feebea47980478b1a68e
Abhishek71h <abhiseema5057@gmail.com>
2025-11-29 13:51:33 +0530

    Left View and Right View Added

---
+++ Tree/TreesViews/rightView.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data) {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* buildTree() {
+    int data;
+    cout << "Enter the data: ";
+    cin >> data;
+
+    if (data == -1) {
+        return NULL;
+    }
+
+    Node* root = new Node(data);
+    cout << "Enter data for left part of " << data << " node:\n";
+    root->left = buildTree();
+
+    cout << "Enter data for right part of " << data << " node:\n";
+    root->right = buildTree();
+    return root;
+}
+
+void printRightView(Node* root) {
+    if (root == NULL) return;
+
+    queue<Node*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        int levelSize = q.size();
+
+        // First node of this level is the left view node
+        for (int i = 0; i < levelSize; i++) {
+            Node* frontNode = q.front();
+            q.pop();
+
+            // take the first element of this level
+            if (i == 0) {
+                cout << frontNode->data << " ";
+            }
+
+            if (frontNode->right) q.push(frontNode->right);
+            if (frontNode->left) q.push(frontNode->left);
+        }
+    }
+}
+
+int main() {
+    Node* root = buildTree();
+    cout << "\nLeft View: ";
+    printRightView(root);
+    return 0;
+}
